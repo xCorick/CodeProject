7ff769e5cacc57138d709e2f807e3ddf953177ed
Corick <corick_serrano12@hotmail.com>
2024-07-24 16:42:54 -0700

    Clase conector y clase usuario agregado

---
+++ Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeProject
+{
+    public class Usuario
+    {
+        private string usuarioCorreo;
+        private string usuarioPasword;
+        private bool usuarioTipo;
+        private string usuarioNombre;
+
+        public string Correo { get => usuarioCorreo; set => usuarioCorreo = value; }
+        public string PassworD { get => usuarioPasword; set => usuarioPasword = value; }
+        public bool Tipo { get => usuarioTipo; set => usuarioTipo = value; }
+        public string UsuarioNombre { get => usuarioNombre; set => usuarioNombre = value; }
+
+        public Usuario(string usuarioCorreo, string usuarioPasword, bool usuarioTipo, string usuarioNombre)
+        {
+            this.usuarioCorreo = usuarioCorreo;
+            this.usuarioPasword = usuarioPasword;
+            this.usuarioTipo = usuarioTipo;
+            this.usuarioNombre = usuarioNombre;
+        }
+    }
+}
