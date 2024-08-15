e790847befd6cb289eafe965847d61dd81de1d2d
Alex5727 <alejandrogrjalva@gmail.com>
2024-08-15 13:21:55 -0700

    tr

--- Scripts/CatalogoParametro.js
+++ Scripts/CatalogoParametro.js
@@ -1,14 +1,7 @@
-﻿function redirigirConId(event) {
-    // Previene el comportamiento predeterminado del botón
-    event.preventDefault();
-
-    // Obtiene el botón que fue clicado
-    const boton = event.target;
-
-    // Obtiene el id del botón
-    const idBoton = boton.id;
-    console.log("El valor es: ", idBoton);
-
-    // Redirige a la otra página con el id como parámetro
-    window.location.href = 'PanProducto.aspx?id=' + encodeURIComponent(idBoton);
+﻿function redirectToPage() {
+    
+    var valueToPass = document.getElementById('inputValue').value;
+    localStorage.setItem('storedValue', valueToPass);
+    
+    window.location.href = 'PanProducto.aspx';
 }
--- Scripts/Recuperacion.js
+++ Scripts/Recuperacion.js
@@ -1,19 +1,7 @@
-﻿function obtenerParametro(nombre) {
-    // Crea un objeto URLSearchParams para manejar la cadena de consulta
-    const params = new URLSearchParams(window.location.search);
-    // Obtiene el valor del parámetro con el nombre dado
-    return params.get(nombre);
-}
-
-window.onload = function () {
-    // Obtiene el valor del parámetro 'id'
-    const idBoton = obtenerParametro('id');
-    // Obtiene el elemento con id 'mensaje'
-    const mensajeElemento = document.getElementById('mensaje');
-    if (mensajeElemento) {
-        // Muestra el valor del parámetro en el elemento con id 'mensaje'
-        mensajeElemento.innerText = 'El id del botón es: ' + idBoton;
-    } else {
-        // Imprime un mensaje de error en la consola si el elemento no se encuentra
-        console.error('Elemento con id "mensaje" no encontrado');
+﻿window.onload = function () {
+    var value = localStorage.getItem('storedValue');
+    if (value) {
+        document.getElementById('displayValue').innerText = value;
+        localStorage.removeItem('storedValue'); // Opcional, si solo deseas un uso único
     }
+}
