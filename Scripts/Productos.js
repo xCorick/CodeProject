42a0f73612cadfbf17396c18f1b6e36fda02f5ff
Alex5727 <alejandrogrjalva@gmail.com>
2024-08-21 00:23:16 -0700

    Pantalla con todos los productos

---
+++ Scripts/Productos.js
@@ -0,0 +1,18 @@
+﻿fetch('/YourController/GetProducts')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('productsContainer');
+
+        data.forEach(product => {
+            const card = `
+                    <div class="card">
+                        <img src="${product.Pro_Imagen}" alt="${product.Pro_Nombre}">
+                        <h3>${product.Pro_Nombre}</h3>
+                        <p>Price: $${product.Pro_Precio}</p>
+                        <a href="details.html?id=${product.Pro_ID}">Ver Detalles</a>
+                    </div>
+                `;
+
+            container.innerHTML += card;
+        });
+    });
